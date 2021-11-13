8a2ce0e82d82f41f7b2ae04e3152e196ca81557a
Maria Lukashova <mariexlv418@gmail.com>
2021-11-13 13:36:35 +0300

    add new files

---
+++ subsequence/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace subsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
