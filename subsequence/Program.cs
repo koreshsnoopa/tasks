5aa91a429ce1d6083533e1750208fd92e783885a
Maria Lukashova <mariexlv418@gmail.com>
2021-11-13 13:39:13 +0300

    add method to find max subsequence

--- subsequence/Program.cs
+++ subsequence/Program.cs
@@ -6,7 +6,45 @@ namespace subsequence
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                lenghthOfMaxUnequalSubsequence(args);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Exeption: {ex.Message}");
+                return;
+            }
+
+            Console.WriteLine($"Length of max subsequence unequal signs: {lenghthOfMaxUnequalSubsequence(args)}");
+        }
+
+        static int lenghthOfMaxUnequalSubsequence(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                throw new ArgumentException("You can input only one string!");
+            }
+
+            string str = args[0].Replace(" ", "").ToLower();
+            int max = 0;
+            int temp = 1;
+
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i] != str[i + 1])
+                {
+                    temp++;
+                    if (temp > max)
+                    {
+                        max = temp;
+                    }
+                    continue;
+                }
+
+                temp = 1;
+            }
+            return max;
         }
     }
 }
