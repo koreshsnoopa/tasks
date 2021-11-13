fd55f63d962605a36c499caac8c49770b17289cd
Maria Lukashova <mariexlv418@gmail.com>
2021-11-13 13:33:36 +0300

    add files

---
+++ epam_tasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace epam_tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
