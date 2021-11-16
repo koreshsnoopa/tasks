e71673592f3c40ec22991671e0a05b27b4e45942
Maria Lukashova <mariexlv418@gmail.com>
2021-11-16 16:33:52 +0300

    add new project

---
+++ flying/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace flying
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
