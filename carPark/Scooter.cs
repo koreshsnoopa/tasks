9ccfb636cffe5dfbb702302d2967d96918456e9f
Maria Lukashova <mariexlv418@gmail.com>
2021-11-20 22:36:28 +0300

    change constructor args

--- carPark/Scooter.cs
+++ carPark/Scooter.cs
@@ -6,15 +6,10 @@ namespace carPark
         public int NumberOfSeats { get; set; }
         public ElectricEngine engine = new ElectricEngine();
 
-        public Scooter(int numberOfWheels, string numberOfChassis,
-            double permissibleLoad, string typeOfTransmission, int numberOfGears,
-            string manufacturer, int numberOfSeats, string serialNumber, double power, double volume) : base(numberOfWheels, numberOfChassis,
-                permissibleLoad, typeOfTransmission, numberOfGears, manufacturer)
+        public Scooter(int numberOfSeats, ElectricEngine engine, Chassis chassis, Transmission transmission)
+            : base(transmission, chassis)
         {
             NumberOfSeats = numberOfSeats;
-            engine.Power = power;
-            engine.Volume = volume;
-            engine.SerialNumber = serialNumber;
         }
 
         public override string ToString()
