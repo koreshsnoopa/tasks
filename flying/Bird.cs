c19f2b260fdcad30dd0ae9354a3179afc20af178
Maria Lukashova <mariexlv418@gmail.com>
2021-11-20 23:18:31 +0300

    add methods in Bird

--- flying/Bird.cs
+++ flying/Bird.cs
@@ -3,20 +3,31 @@ namespace flying
 {
     public class Bird : Flyable, IFlyable
     {
-        //public static double Speed = 50;
+        public const double MaxFlightTime = 4;
+
+        public static Random rand = new Random();
+        public int Speed { get; set; }
 
         public Bird(double x, double y, double z) : base(x, y, z)
         {
+            Speed = rand.Next(0, 51);
         }
 
         public void FlyTo(Cordinate3D coordinateFlyTo)
         {
-            throw new NotImplementedException();
+            if (GetFlyTime(coordinateFlyTo) > MaxFlightTime)
+            {
+                throw new ArgumentException($"Bird can't fly longer then {MaxFlightTime} hours!");
+            }
+
+            currentPosition.x = coordinateFlyTo.x;
+            currentPosition.y = coordinateFlyTo.y;
+            currentPosition.z = coordinateFlyTo.z;
         }
 
         public double GetFlyTime(Cordinate3D coordinateFlyTo)
         {
-            throw new NotImplementedException();
+            return GetFlightDistance(coordinateFlyTo) / Speed;
         }
     }
 }
