f34db00df4c24eba1e8898d6818846a100c93504
Maria Lukashova <mariexlv418@gmail.com>
2021-11-23 00:09:45 +0300

    add comments

--- flying/Bird.cs
+++ flying/Bird.cs
@@ -3,9 +3,9 @@ namespace flying
 {
     public class Bird : Flyable, IFlyable
     {
-        public const double MaxFlightTime = 4;
-        public const int MaxSpeed = 20;
-        public const int MinSpeed = 0;
+        public const double MaxFlightTime = 4; // Maximum flight time
+        public const int MaxSpeed = 20; // Maximum speed
+        public const int MinSpeed = 0; // Minimum speed
 
         public int Speed { get; set; }
 
@@ -14,6 +14,11 @@ namespace flying
             Speed = SpeedGenerator();
         }
 
+        /// <summary>
+        /// Method changing the current position to position to fly.
+        /// Bird can't fly longer then 4 hours that is why if flight time more than max flight time method trows Argument exeption
+        /// </summary>
+        /// <param name="coordinateFlyTo"></param>
         public void FlyTo(Cordinate3D coordinateFlyTo)
         {
             if (GetFlyTime(coordinateFlyTo) > MaxFlightTime)
@@ -26,12 +31,21 @@ namespace flying
             currentPosition.z = coordinateFlyTo.z;
         }
 
+        /// <summary>
+        /// Method generate speed between min and max value of it.
+        /// </summary>
+        /// <returns> Speed </returns>
         private int SpeedGenerator()
         {
             Random rand = new Random();
             return rand.Next(MinSpeed, MaxSpeed + 1);
         }
 
+        /// <summary>
+        /// Mathod counting flight time.
+        /// </summary>
+        /// <param name="coordinateFlyTo"></param>
+        /// <returns> Flight time </returns>
         public double GetFlyTime(Cordinate3D coordinateFlyTo)
         {
             return GetFlightDistance(coordinateFlyTo) / Speed;
