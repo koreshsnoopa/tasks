30261546fcb2ea2d559caa87d3e4365266d3b907
Maria Lukashova <mariexlv418@gmail.com>
2021-11-24 23:38:29 +0300

    change getTime method

--- flying/Dron.cs
+++ flying/Dron.cs
@@ -3,17 +3,25 @@ namespace flying
 {
     public class Dron : Flyable, IFlyable
     {
-        public const int MaxFlightDistance = 1000; // Maximum flight distance
+        private const int MaxFlightDistance = 1000; // Maximum flight distance in km
+        private const int MaxSpeed = 200; // Maximum speed in km/h
+        private const int StopTime = 1; // Stop time in minutes
+        private const int Interval = 10; // Interval of stops in minutes
 
-        public static double Speed = 50;
+        public static double Speed { get; private set; }
 
-        public Dron(double x, double y, double z) : base(x, y, z)
+        public Dron(double x, double y, double z, double speed) : base(x, y, z)
         {
+            if (speed == 0 || speed > MaxSpeed)
+            {
+                throw new ArgumentException($"Speed must be less then {MaxSpeed} km/h and morethen 0 km/h!");
+            }
+            Speed = speed;
         }
 
         /// <summary>
         /// Method changing the current position to position to fly.
-        /// Dron can't fly far rhen 1000 km that is why if flight distance more than 1000 km method trows Argument exeption
+        /// Dron can't fly far then 1000 km that is why if flight distance more than 1000 km method trows Argument exeption
         /// </summary>
         /// <param name="coordinateFlyTo"></param>
         public void FlyTo(Cordinate3D coordinateFlyTo)
@@ -35,7 +43,9 @@ namespace flying
         /// <returns> Flight time </returns>
         public double GetFlyTime(Cordinate3D coordinateFlyTo)
         {
-            return ((int)(GetFlightDistance(coordinateFlyTo) / Speed * 6)) / 60.0 + GetFlightDistance(coordinateFlyTo) / Speed;
+            double time = GetFlightDistance(coordinateFlyTo) / Speed;
+
+            return (int)(time / (Interval / 60) * (StopTime / 60)) + time; // /60 to convert minutes to hours
         }
     }
 }
