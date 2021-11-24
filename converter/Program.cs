997c74158cfeef65f0962b8423abeb4b7c7bd2f4
Maria Lukashova <mariexlv418@gmail.com>
2021-11-24 13:39:23 +0300

    change converter method

--- converter/Program.cs
+++ converter/Program.cs
@@ -37,31 +37,27 @@ namespace converter
 
         static string Converter(int number, int basis)
         {
-            if (number == 1)
+            int size = number > 0 ? (int)Math.Log(number, basis) + 1 : 1;
+
+            char[] res = new char[size];
+
+            int i = 0;
+
+            do
             {
-                return "1";
-            }
-
-            if (number == 0)
-            {
-                return "0";
-            }
-
-            char[] res = new char[(int)Math.Log(number, basis) + 1];
-
-            int i = 1;
-
-            while (number != 1)
-            {
-                res[res.Length - i] = number % basis < 10 ? (char)(48 + number % basis) : (char)(55 + number % basis); // 48 and 55 is ASCII
-                number /= basis;
-                i++;
-                if (i == res.Length)
+                if (i + 1 == res.Length)
                 {
-                    res[res.Length - i] = number % basis < 10 ? (char)(48 + number) : (char)(55 + number); // 48 and 55 is ASCII
+                    res[i] = number % basis < 10 ? (char)(48 + number) : (char)(55 + number); // 48 and 55 is ASCII
                     break;
                 }
+
+                res[i] = number % basis < 10 ? (char)(48 + number % basis) : (char)(55 + number % basis); // 48 and 55 is ASCII
+                number /= basis;
+                i++;
             }
+            while (number != 0);
+
+            Array.Reverse(res);
 
             return new string(res);
         }
