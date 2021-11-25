7fc3eddbb50614715f18a9ef75c8d6fcae562241
Maria Lukashova <mariexlv418@gmail.com>
2021-11-25 19:03:30 +0300

    add engine

--- carPark/Transport.cs
+++ carPark/Transport.cs
@@ -3,13 +3,15 @@ namespace carPark
 {
     public abstract class Transport
     {
-        public Transmission transmission = new Transmission();
-        public Chassis chassis = new Chassis();
+        public Transmission transmission;
+        public Chassis chassis;
+        public Engine engine;
 
-        public Transport(Transmission transmission, Chassis chassis)
+        public Transport(Transmission transmission, Chassis chassis, Engine engine)
         {
             this.transmission = transmission;
             this.chassis = chassis;
+            this.engine = engine;
         }
 
     }
