9713faf55466e320a76a63032b1e21942be3f7fd
Maria Lukashova <mariexlv418@gmail.com>
2022-01-07 19:14:48 +0300

    add new project

---
+++ Locators/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Locators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.bbc.com/");
+            
+           
+        }
+    }
+}
