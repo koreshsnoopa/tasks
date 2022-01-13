5439f9bf77de21c2c61dabf32376ea2c2e20180c
Maria Lukashova <mariexlv418@gmail.com>
2022-01-13 18:26:44 +0300

    add new class

---
+++ Locators/NewsPage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Locators
+{
+    public class ТуцыЗфпу
+    {
+        public ТуцыЗфпу()
+        {
+        }
+    }
+}
--- Locators/Program.cs
+++ Locators/Program.cs
@@ -10,8 +10,12 @@ namespace Locators
         {
             IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://www.bbc.com/");
-            
-           
+            //IWebElement webElement = driver.FindElement(By.Id("orb-search-q"));
+            IWebElement webElement = driver.FindElement(By.LinkText("Sport"));
+           // IWebElement webElement = driver.FindElement(By.XPath());
+           // webElement.SendKeys("Sport");
+            webElement.Click();
+
         }
     }
 }
